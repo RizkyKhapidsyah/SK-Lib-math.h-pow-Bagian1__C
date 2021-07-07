4ddb1d4b437017a53e63cb1304488656c48a33be
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-07 20:56:49 +0700

    Add project files.

---
+++ SK-Lib-math.h-pow-Bagian1__C/Source.c
@@ -0,0 +1,11 @@
+#include <stdio.h>      /* printf */
+#include <math.h>       /* pow */
+#include <conio.h>
+
+int main() {
+	printf("9 ^ 2 = %f\n", pow(9.0, 2.0));
+	printf("8.12 ^ 8 = %f\n", pow(8.12, 8));
+	printf("76.09 ^ 4.32 = %f\n", pow(76.09, 4.32));
+	_getch();
+	return 0;
+}
